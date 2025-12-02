f5d47baaa417405cfcf8a172410f2cc144f7650d
jermzblake <jermzblake@gmail.com>
2025-12-02 16:28:55 -0700

    refactor server file to serve static assets

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 import 'dotenv/config'
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/bun'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use('/*', serveStatic({ root: './public' }))
+
+// app.get('/', (c) => {
+//   return c.text('Hello Hono!')
+// })
+app.get('/', (c) =>
+  c.html(`<html>
+  <head>
+    <link rel="stylesheet" href="/style.css">
+  </head>
+  <body class="bg-red-100">
+    Hello Tailwind v4 + Hono + HTMX
+  </body>
+</html>`),
+)
+
+app.get('/ping', (c) => c.text('pong'))
 
 export default {
   port: 4000,
