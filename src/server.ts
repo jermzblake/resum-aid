64c72da9470437ad3809ddc72bc6d6f5f56ddd1b
jermzblake <jermzblake@gmail.com>
2025-12-03 16:52:51 -0700

    add dependency injection to server

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,27 @@ import { registerLandingRoute } from './routes/landing.route'
 import { registerJobMatchRoute } from './routes/job-matcher.route'
 import { registerBulletAnalyzerRoute } from './routes/bullet-analyzer.route'
 import { registerResumeBuilderRoute } from './routes/resume-builder.route'
+import { LLMFactory } from './providers/llm-factory'
+import type { LLMService } from '@/services/llm/llm.service'
+import { LLMTaskService } from '@/services/llm/llm.task.service'
+import { JobMatcherController } from '@/controllers/job-matcher.controller'
+import { JobMatcherService } from '@/services/job-matcher-service'
 
 export const createApp = () => {
   const app = new Hono()
 
   app.use('/*', serveStatic({ root: './public' }))
 
+  // Dependencies
+  const llmService: LLMService = LLMFactory.createOllama()
+  const llmTaskService = new LLMTaskService(llmService)
+  const jobMatcherService = new JobMatcherService(llmTaskService)
+
+  const jobMatcherController = new JobMatcherController(jobMatcherService)
+
+  // Mount routes
   registerLandingRoute(app)
-  registerJobMatchRoute(app)
+  registerJobMatchRoute(app, jobMatcherController)
   registerBulletAnalyzerRoute(app)
   registerResumeBuilderRoute(app)
 
