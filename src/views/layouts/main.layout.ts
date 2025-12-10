8d468626cdc7b22c2b7a61fb84066a6c6e9f192e
jermzblake <jermzblake@gmail.com>
2025-12-10 16:34:36 -0700

    update page title

--- src/views/layouts/main.layout.ts
+++ src/views/layouts/main.layout.ts
@@ -7,7 +7,7 @@ export const MainLayout = (content: string, activeTab: string = 'home') => html`
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Resume Helper</title>
+      <title>Resume Tools - Resum-Aid</title>
       <link href="/style.css" rel="stylesheet" />
       <script src="https://unpkg.com/htmx.org@1.9.10"></script>
       <script src="https://unpkg.com/htmx.org@1.9.12/dist/ext/sse.js"></script>
